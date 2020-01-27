d126d21ea970ecec7dae678d39316292b171a5aa
Andrea Sacerdoti <andrea.sacerdoti@mobilesoft.it>
2020-01-27 17:12:18 +0100

    Make RequestConfiguration fields non-settable

--- Sources/Requests/DefaultRequestConfiguration.swift
+++ Sources/Requests/DefaultRequestConfiguration.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 public protocol RequestConfiguration {
-  var defaultHeaders: [String: String] { get set }
-  var requestProtocol: String { get set }
-  var baseURL: String { get set }
-  var authenticationRealm: String { get set }
-  var authenticationMethod: String { get set }
+  var defaultHeaders: [String: String] { get }
+  var requestProtocol: String { get }
+  var baseURL: String { get }
+  var authenticationRealm: String { get }
+  var authenticationMethod: String { get }
   var protectionSpace: URLProtectionSpace { get }
 }
 
@@ -24,20 +24,29 @@ public extension RequestConfiguration {
 
 open class DefaultRequestConfiguration: RequestConfiguration {
 
-  open var defaultHeaders: [String: String] = [
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-  ]
+  open var defaultHeaders: [String: String] {
+    [
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    ]
+  }
 
-  open var requestProtocol = Bundle.main.infoDictionary?["RequestProtocol"] as? String ?? "https"
+  open var requestProtocol: String {
+    Bundle.main.infoDictionary?["RequestProtocol"] as? String ?? "https"
+  }
 
   /// - Important: This must be set in the app's **Info.plist**.
-  open var baseURL = Bundle.main.infoDictionary?["BaseURL"] as? String ?? "test.url.com"
+  open var baseURL: String {
+    Bundle.main.infoDictionary?["BaseURL"] as? String ?? "test.url.com"
+  }
 
-  open var authenticationRealm = Bundle.main.infoDictionary?["AuthenticationRealm"] as? String ?? "Restricted"
+  open var authenticationRealm: String {
+    Bundle.main.infoDictionary?["AuthenticationRealm"] as? String ?? "Restricted"
+  }
 
-  open var authenticationMethod = NSURLAuthenticationMethodDefault
-
+  open var authenticationMethod: String {
+    NSURLAuthenticationMethodDefault
+  }
   
   public var protectionSpace: URLProtectionSpace {
     URLProtectionSpace(host: baseURL,
