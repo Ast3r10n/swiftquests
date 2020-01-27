dcb01a8aeccd320da45c61bae8464b4597f4a72e
Andrea Sacerdoti <andrea.sacerdoti@mobilesoft.it>
2020-01-27 14:17:07 +0100

    Allow setting RequestConfiguration properties

--- Sources/Requests/RequestConfiguration.swift
+++ Sources/Requests/RequestConfiguration.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 open class RequestConfiguration {
-  public static var `default` = RequestConfiguration()
+  open static var `default` = RequestConfiguration()
 
-  open var defaultHeaders: [String: String] {
+  open var defaultHeaders: [String: String] =
     [
       "Accept": "application/json",
       "Content-Type": "application/json",
-    ]
-  }
+  ]
+
   
   open var requestProtocol: String = Bundle.main.infoDictionary?["RequestProtocol"] as? String ?? "https"
 
@@ -23,15 +23,12 @@ open class RequestConfiguration {
   open var baseURL: String = Bundle.main.infoDictionary?["BaseURL"] as? String ?? "test.url.com"
 
 
-  open var authenticationRealm: String {
-    Bundle.main.infoDictionary?["AuthenticationRealm"] as? String ?? "Restricted"
-  }
+  open var authenticationRealm: String = Bundle.main.infoDictionary?["AuthenticationRealm"] as? String ?? "Restricted"
 
-  open var defaultProtectionSpace: URLProtectionSpace {
-    URLProtectionSpace(host: baseURL,
-                       port: 443,
-                       protocol: requestProtocol,
-                       realm: authenticationRealm,
-                       authenticationMethod: NSURLAuthenticationMethodDefault)
-  }
+  
+  open var defaultProtectionSpace = URLProtectionSpace(host: baseURL,
+                                                       port: 443,
+                                                       protocol: requestProtocol,
+                                                       realm: authenticationRealm,
+                                                       authenticationMethod: NSURLAuthenticationMethodDefault)
 }
