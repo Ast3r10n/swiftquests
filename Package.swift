4cb0a781861143d4782955912ec39c8285983a9f
Andrea Sacerdoti <andrea.sacerdoti@adevinta.com>
2022-09-30 12:00:52 +0200

    Add async perform
    Remove unused variable names

--- Package.swift
+++ Package.swift
@@ -3,18 +3,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftQuests",
-  products: [
-    .library(
-      name: "SwiftQuests",
-      targets: ["SwiftQuests"]),
-  ],
-  targets: [
-    .target(
-      name: "SwiftQuests",
-      dependencies: []),
-    .testTarget(
-      name: "SwiftQuestsTests",
-      dependencies: ["SwiftQuests"]),
-  ]
+    name: "SwiftQuests",
+    products: [
+        .library(
+            name: "SwiftQuests",
+            targets: ["SwiftQuests"]),
+    ],
+    targets: [
+        .target(name: "SwiftQuests"),
+        .testTarget(
+            name: "SwiftQuestsTests",
+            dependencies: ["SwiftQuests"]),
+    ]
 )
